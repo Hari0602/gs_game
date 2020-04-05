d5eaa2cd4cff1b08317bded904c7eaca22f99fbd
Hari0602 <harikrishna0602@email.com>
2020-04-05 15:57:24 +0530

    all the files are created again

---
+++ guessinggame.sh
@@ -0,0 +1,19 @@
+function count {
+  count1=$(find . -maxdepth 1 -type f| wc -l)
+  echo $count1
+}
+
+count2=$(count)
+echo "Hello welcome to the game"
+echo "Guess the number of files in my current directory, please"
+read a
+
+while [[ $count2 -ne $a ]]
+do
+  if [[ $count2 -gt $a ]]
+  then
+    echo "It's too small! Try another number, please"
+    read a
+  elif [[ $count2 -lt $a ]]
+  then
+    echo "It's too much! Try another number, please"
